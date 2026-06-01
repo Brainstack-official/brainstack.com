06aeb006e33b390a7008bdb7e3cf1eddf7731fa5
Brainstack-official <brainstack.com@gmail.com>
2026-06-01 20:30:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ const resources = [
 {
 title:"Mechanical engineering complete guide",
 keywords:["Mechanical engineering","Engineering","engineering"],
-url:"pdfs/python-notes.pdf"
+url:"../engineering-hub/Mechanical_Engineering_Guide.pdf"
 },
 
 {
