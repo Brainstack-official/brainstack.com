fdcd7df49dc40741e2205ce016f1ed852b5617d2
Brainstack-official <brainstack.com@gmail.com>
2026-06-08 23:00:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
-// SMOOTH APPEAR ANIMATION
+import { createClient }
+from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
+const supabase = createClient(
+  "https://waeeeyktjujpvpcprhbv.supabase.co",
+  "sb_publishable_4HE-L3GRIkDq4wSIaxWyJw_vtngeHdj"
+);
+
+// Route protection
+const {
+  data: { session }
+} = await supabase.auth.getSession();
+
+if (!session) {
+  window.location.href = "login.html";
+}
+
+// Show profile photo if it exists
+const userPhoto = document.getElementById("userPhoto");
+
+if (session && userPhoto) {
+  userPhoto.src =
+    session.user.user_metadata.avatar_url ||
+    session.user.user_metadata.picture ||
+    "https://cdn-icons-png.flaticon.com/512/3135/3135715.png";
+}
+
+// SMOOTH APPEAR ANIMATION
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
@@ -9,7 +35,6 @@ const observer = new IntersectionObserver((entries) => {
 });
 
 // OBSERVE RESOURCE CARDS
-
 const cards = document.querySelectorAll(".resource-card");
 
 cards.forEach((card) => {
