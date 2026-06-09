1cf6939890a3bcac6f896b83b78c5a17d88e0366
Brainstack-official <brainstack.com@gmail.com>
2026-06-09 12:51:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 // SMOOTH APPEAR ANIMATION
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
@@ -13,4 +12,27 @@ const cards = document.querySelectorAll(".resource-card");
 
 cards.forEach((card) => {
   observer.observe(card);
+});
+document.querySelectorAll(".pdf-link").forEach(link => {
+
+  link.addEventListener("click", async () => {
+
+    const {
+      data: { user }
+    } = await supabase.auth.getUser();
+
+    if (!user) return;
+
+    await supabase
+      .from("downloads")
+      .insert([
+        {
+          user_id: user.id,
+          pdf_name: link.parentElement.querySelector("h3").innerText,
+          pdf_link: link.href
+        }
+      ]);
+
+  });
+
 });
