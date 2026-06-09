0926626e6ad563a4a8cdfbe2dfcb3b34a3021eb4
Brainstack-official <brainstack.com@gmail.com>
2026-06-09 13:06:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,26 +13,3 @@ const cards = document.querySelectorAll(".resource-card");
 cards.forEach((card) => {
   observer.observe(card);
 });
-document.querySelectorAll(".pdf-link").forEach(link => {
-
-  link.addEventListener("click", async () => {
-
-    const {
-      data: { user }
-    } = await supabase.auth.getUser();
-
-    if (!user) return;
-
-    await supabase
-      .from("downloads")
-      .insert([
-        {
-          user_id: user.id,
-          pdf_name: link.parentElement.querySelector("h3").innerText,
-          pdf_link: link.href
-        }
-      ]);
-
-  });
-
-});
