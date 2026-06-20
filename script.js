2b7a1fb883590d3d7d0a7d90427cc9198f79e9ec
Brainstack-official <brainstack.com@gmail.com>
2026-06-20 12:32:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,28 @@ const cards = document.querySelectorAll(".resource-card");
 cards.forEach((card) => {
   observer.observe(card);
 });
+
+
+async function viewPDF(resourceName, pdfUrl) {
+
+    const {
+        data: { user }
+    } = await supabase.auth.getUser();
+
+    if(user){
+
+        await supabase
+        .from("pdf_views")
+        .insert([
+            {
+                user_id: user.id,
+                resource_name: resourceName,
+                resource_url: pdfUrl
+            }
+        ]);
+
+    }
+
+    window.open(pdfUrl, "_blank");
+}
+
